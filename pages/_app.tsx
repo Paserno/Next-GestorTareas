f1a61be79dd526f1ce9e4150b7fcb80b51568fc6
Paserno <paserno.x@gmail.com>
2023-07-25 00:44:33 -0400

    Temas de material-UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { lightTheme, darkTheme } from '../themes'
+
+
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+
+  )
 }
 
 export default MyApp
