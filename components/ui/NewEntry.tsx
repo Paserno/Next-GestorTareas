6532c981bd5712ea301bd910a558631fb6d66c9d
Paserno <paserno.x@gmail.com>
2023-07-28 00:54:59 -0400

    Formulario y estado de entries

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,3 +1,4 @@
+import { useState, ChangeEvent } from 'react';
 import { Button, Box, TextField } from '@mui/material';
 
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
@@ -5,47 +6,75 @@ import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOu
 
 
 export const NewEntry = () => {
+
+    const [isAdding, setIsAdding] = useState(false);
+
+    const [inputValue, setInputValue] = useState('');
+    const [touched, setTouched] = useState(false);
+
+    const onTextFieldChanges = (event: ChangeEvent<HTMLInputElement> ) => {
+        setInputValue(event.target.value);
+    }
+
+    const onSave = () => {
+        if( inputValue.length === 0) return;
+
+        console.log({inputValue});
+    }
+
+
     return (
-        <Box sx={{ marginBottom: 2, paddingX: 2}}>
+        <Box sx={{ marginBottom: 2, paddingX: 2 }}>
 
-            <Button 
-                startIcon={<AddCircleOutlineOutlinedIcon />}
-                fullWidth
-                variant='outlined'
-                // size='large'
-            >
-                Agregar Tarea
-            </Button>
+            {
+                isAdding ? (
+                    <>
 
+                        <TextField
+                            fullWidth
+                            sx={{ marginTop: 2, marginBottom: 1 }}
+                            placeholder='Nueva Entrada'
+                            autoFocus
+                            multiline
+                            label='Nueva Entrada'
+                            helperText={ inputValue.length <= 0 && touched && 'Ingrese un valor'}
+                            error={inputValue.length <= 0 && touched}
+                            value={inputValue}
+                            onChange={onTextFieldChanges}
+                            onBlur={ () => setTouched(true) }
+                        />
 
-            <TextField 
-                fullWidth
-                sx={{ marginTop: 2, marginBottom: 1}}
-                placeholder='Nueva Entrada'
-                autoFocus
-                multiline
-                label='Nueva Entrada'
-                helperText='Ingrese un valor'
-            />
+                        <Box display='flex' justifyContent={'space-between'}>
+                            <Button
+                                variant='text'
+                                color='error'
+                                onClick={() => setIsAdding(false)}
+                            >
+                                Cancelar
+                            </Button>
+                            <Button
+                                variant='outlined'
+                                color='info'
+                                endIcon={<SaveOutlinedIcon />}
+                                onClick={onSave}
+                            >
+                                Guardar
+                            </Button>
 
-            <Box display='flex' justifyContent={'space-between'}>
-                <Button
-                    variant='text'
-                    color='error'
-                >
-                    Cancelar
-                </Button>
-                <Button
-                    variant='outlined'
-                    color='info'
-                    endIcon={<SaveOutlinedIcon />}
-
-                >
-                    Guardar
-                </Button>
-
-            </Box>
-
+                        </Box>
+                    </>
+                )
+                    : (
+                        <Button
+                            startIcon={<AddCircleOutlineOutlinedIcon />}
+                            fullWidth
+                            variant='outlined'
+                            onClick={() => setIsAdding(true)}
+                        >
+                            Agregar Tarea
+                        </Button>
+                    )
+            }
         </Box>
     )
 }
