3d4c1231cb5e54af4cde4cd2e9a42c74f1f75d02
Paserno <paserno.x@gmail.com>
2023-07-28 20:11:24 -0400

    Cambiar estado Entry

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -45,14 +45,18 @@ export const EntriesProvider: FC<PropsWithChildren> = ({ children }) => {
             status: 'pending'
         }
 
-        dispatch({ type: '[Entry] - Add-Entry', payload: newEntry});
+        dispatch({ type: '[Entry] - Add-Entry', payload: newEntry });
+    }
 
+    const updateEntry = (entry: Entry) => {
+        dispatch({ type: '[Entry] - Update-Entry', payload: entry });
     }
 
     return (
         <EntriesContext.Provider value={{
             ...state,
-            addNewEntry
+            addNewEntry,
+            updateEntry,
         }}>
             {children}
         </EntriesContext.Provider>
